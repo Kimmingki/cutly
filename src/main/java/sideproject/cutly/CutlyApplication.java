5640957e79170bac8674e49cb0f80533220c30a5
Kimmingki <a01050919568@gmail.com>
2025-02-07 10:25:41 +0900

    Setup Project

---
+++ src/main/java/sideproject/cutly/CutlyApplication.java
@@ -0,0 +1,13 @@
+package sideproject.cutly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sideproject/cutly/CutlyApplicationTests.java
@@ -0,0 +1,13 @@
+package sideproject.cutly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
