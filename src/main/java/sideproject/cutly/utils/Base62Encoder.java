9de40681df10a81a632c70af9c5f55789c90651c
Kimmingki <a01050919568@gmail.com>
2025-02-10 21:56:25 +0900

    base62 로직 구현

--- src/main/java/sideproject/cutly/utils/Base62Encoder.java
+++ src/main/java/sideproject/cutly/utils/Base62Encoder.java
@@ -12,6 +12,22 @@ public class Base62Encoder {
 
     // 숫자를 Base62 문자열로 변환
     public static String encode(long value) {
+        StringBuilder sb = new StringBuilder();
+        while (value > 0) {
+            sb.insert(0, BASE62.charAt((int) (value % 62)));
+            value /= 62;
+        }
 
+        return sb.toString();
+    }
+
+    // 랜덤한 Base62 문자열 생성 (최대 10자리)
+    public static String generateRandomCode() {
+        StringBuilder sb = new StringBuilder(10);
+        for (int i = 0; i < 8; i++) {
+            sb.append(BASE62.charAt(RANDOM.nextInt(62)));
+        }
+
+        return sb.toString();
     }
 }
